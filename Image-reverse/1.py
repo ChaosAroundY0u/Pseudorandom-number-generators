d348239d6595ff01559c992f7dc4ebf375cf59e1
ChaosAroundY0u <152779643+ChaosAroundY0u@users.noreply.github.com>
2024-03-18 18:47:06 +0300

    reverse image added

---
+++ Image-reverse/1.py
@@ -0,0 +1,19 @@
+import random
+from PIL import Image, ImageDraw
+
+image = Image.open("perun.jpg")
+draw = ImageDraw.Draw(image)
+width = image.size[0]
+height = image.size[1]
+pix = image.load()
+
+# print(width, height)
+reverse_image = Image.new('RGB', (width, height))
+for i in range(width):
+    for j in range(height):
+        pix = image.getpixel((i, j))
+        reverse_image.putpixel((width - i - 1, j), pix)
+
+image.save("perun_reversed.jpg")
+image.show("perun_reversed.jpg")
+
